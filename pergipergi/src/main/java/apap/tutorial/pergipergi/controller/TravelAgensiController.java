1e73068f414bd64903348e373a9d8464f2338e29
Kamila Kaffah <kaffahkamila@gmail.com>
2021-09-11 18:00:28 +0700

    fixed minor

--- pergipergi/src/main/java/apap/tutorial/pergipergi/controller/TravelAgensiController.java
+++ pergipergi/src/main/java/apap/tutorial/pergipergi/controller/TravelAgensiController.java
@@ -5,6 +5,7 @@ import apap.tutorial.pergipergi.service.TravelAgensiService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -39,7 +40,7 @@ public class TravelAgensiController {
         return "add-agensi";
     }
 
-    @RequestMapping("agensi/viewAll")
+    @RequestMapping("/agensi/viewAll")
     public String listAgensi(Model model){
         //Mendapatkan semua TravelAgensiModel
         List<TravelAgensiModel> listAgensi = travelAgensiService.getListAgensi();
@@ -51,7 +52,7 @@ public class TravelAgensiController {
         return "viewall-agensi";
     }
 
-    @RequestMapping("agensi/view")
+    @RequestMapping("/agensi/view")
     public String detailAgensi(
             @RequestParam(value = "idAgensi") String idAgensi,
             Model model
@@ -66,6 +67,23 @@ public class TravelAgensiController {
     }
 
     //Latihan
+    @GetMapping(value= "/agensi/view/id-agensi/{idAgensi}")
+    public String viewAgensiWithPathVariable(
+            @PathVariable(value = "idAgensi") String idAgensi,
+            Model model
+    ){
+        // Mendapatkan TravelAgensiModel sesuaidengan idAgensi
+        TravelAgensiModel agensi = travelAgensiService.getAgensiByidAgensi(idAgensi);
+
+        if (agensi == null) {
+            return "notfound";
+        }else{
+            //Add variabel TravelAgensiModel ke 'agensi' untuk dirender pada thymeleaf
+            model.addAttribute("agensi", agensi);
+            return "view-agensi";
+        }
+    }
+
     @RequestMapping("/agensi/update/id-agensi/{idAgensi}/no-telepon/{noTelepon}")
     public String updateWithPathVariable(
             @PathVariable(value = "idAgensi", required = true) String idAgensi,
